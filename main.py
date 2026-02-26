19fd2e5cf4837f304f0631e830fe65bb5ebc7e3b
shishir kr <shishir.raj2@gmail.com>
2026-02-27 01:13:48 +0530

    basic structure only

--- main.py
+++ main.py
@@ -6,21 +6,21 @@ from training.train_sentiment import train
 
 
 def main():
-    processed_data = pd.DataFrame(clean_text())
-    save_csv('./data/processed/processed_data.csv', processed_data)
-    data = pd.read_csv('./data/processed/processed_data.csv')
-    print(data['sentiment'].value_counts())
-
-    label_map = {
-        "negative": 0,  # SELL
-        "neutral": 1,  # HOLD
-        "positive": 2  # BUY
-    }
-
-    data["label"] = data["sentiment"].map(label_map)
-    print(data.head())
-    data.to_csv('./data/processed/processed_data_labeled.csv', index=False)
-    train()
+    # processed_data = pd.DataFrame(clean_text())
+    # save_csv('./data/processed/processed_data.csv', processed_data)
+    # data = pd.read_csv('./data/processed/processed_data.csv')
+    # print(data['sentiment'].value_counts())
+    #
+    # label_map = {
+    #     "negative": 0,  # SELL
+    #     "neutral": 1,  # HOLD
+    #     "positive": 2  # BUY
+    # }
+    #
+    # data["label"] = data["sentiment"].map(label_map)
+    # print(data.head())
+    # data.to_csv('./data/processed/processed_data_labeled.csv', index=False)
+    # train()
     from inference.predict import predict
     print(predict("Tesla earnings look weak this quarter"))
     # predict("Tesla earnings look weak this quarter")
