ff6f77697771c5945fe4333535f40f4fbbe583d1
shishir kr <shishir.raj2@gmail.com>
2026-02-27 01:49:07 +0530

    dockerise

--- main.py
+++ main.py
@@ -1,33 +1,23 @@
-# This is a sample Python script.
-import pandas as pd
-from preprocessing.save_file import save_csv
-from preprocessing.text_cleaner import clean_text
-from training.train_sentiment import train
+# main.py
 
+from fastapi import FastAPI
+from pydantic import BaseModel
+from inference.predict import load_model, predict
 
-def main():
-    # processed_data = pd.DataFrame(clean_text())
-    # save_csv('./data/processed/processed_data.csv', processed_data)
-    # data = pd.read_csv('./data/processed/processed_data.csv')
-    # print(data['sentiment'].value_counts())
-    #
-    # label_map = {
-    #     "negative": 0,  # SELL
-    #     "neutral": 1,  # HOLD
-    #     "positive": 2  # BUY
-    # }
-    #
-    # data["label"] = data["sentiment"].map(label_map)
-    # print(data.head())
-    # data.to_csv('./data/processed/processed_data_labeled.csv', index=False)
-    # train()
-    from inference.predict import predict
-    print(predict("Tesla earnings look weak this quarter"))
-    # predict("Tesla earnings look weak this quarter")
+app = FastAPI(title="Stock Sentiment API")
 
+class TextRequest(BaseModel):
+    text: str
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    main()
+@app.on_event("startup")
+def startup_event():
+    load_model()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+@app.post("/predict")
+def get_prediction(request: TextRequest):
+    result = predict(request.text)
+    return result
+
+@app.get("/")
+def health():
+    return {"status": "ok"}
